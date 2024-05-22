d1d248ccec79274d2e46651de230283036094586
MichailKaa <unionfalse@gmail.com>
2024-05-23 02:28:57 +0300

    >> 512K test 4.30 OK

--- CPLD_J/src/main.v
+++ CPLD_J/src/main.v
@@ -98,7 +98,7 @@ wire n_rom_rd = CPU_RD | CPU_MREQ;//   | n_rom_cs;
 
 assign ROM_A14 = rombank;
 assign ROM_A15 = 1'b1;
-assign ROM_A16 = 1'b0;
+assign ROM_A16 = 1'b1;
 assign ROM_A17 = 1'b0;
 assign ROM_A18 = 1'b0;
 assign WR_ROM 	= 1'b1;
@@ -131,7 +131,7 @@ wire main_ram_cs =  cpu_or_dis? (CPU_MREQ | ~n_cpu_a_0000_3fff):(1'b0);
 wire main_ram_rd =  cpu_or_dis? (CPU_RD   | main_ram_cs)			:(n_vrd);
 wire main_ram_wr =  cpu_or_dis? (CPU_WR   | main_ram_cs)			:(1'b1);
 
-assign MA = cpu_or_dis? ((A[15] & A[14]) ?({2'b11, rambank, A[13:0]}):({2'b11, A[14], A[15:0]}))    :({3'b111, vbank, screen_addr});
+assign MA = cpu_or_dis? ((A[15] & A[14]) ?({ext_rambank_7ffd, rambank, A[13:0]}):({2'b11, A[14], A[15:0]}))    :({3'b111, vbank, screen_addr});
 
 assign D  = cpu_or_dis? ((main_ram_rd == 1'b0) ? MD : 8'bZ)		:(8'bZ);
 assign MD = cpu_or_dis? ((main_ram_wr == 1'b0) ? D  : 8'bZ)		:(8'bZ);
@@ -287,7 +287,11 @@ end
 
 /* PORT #7FFD */
 wire port_7ffd_cs = CPU_M1 == 1 && n_iorq0 == 0 && A == 16'h7ffd;
+
+
 reg [2:0] rambank;
+reg [1:0] ext_rambank_7ffd;
+
 reg rombank, vbank, lock_7ffd;
 always @(posedge CLK_14MHZ or negedge CPU_RESET) begin
     if (!CPU_RESET) begin
@@ -295,12 +299,45 @@ always @(posedge CLK_14MHZ or negedge CPU_RESET) begin
         vbank 		<= 0;
         rombank 	<= 0;
         lock_7ffd <= 0;
+		  ext_rambank_7ffd <= 2'b11;  
     end
-    else if (port_7ffd_cs && CPU_WR == 0 && lock_7ffd == 0) begin
-        rambank 	<= D[2:0];
-        vbank 		<= D[3];
-        rombank 	<= D[4];
-        lock_7ffd <= D[5];
+    else begin 
+		if (port_7ffd_cs && CPU_WR == 0 && lock_7ffd == 0) begin
+        rambank 					<= D[2:0];
+        vbank 						<= D[3];
+        rombank 					<= D[4];
+        //lock_7ffd 				<= lock128k?1'b0: D[5];       :)
+		  //ext_rambank_7ffd[0] 	<= lock128k?1'b1:~D[6];
+		  //ext_rambank_7ffd[1] 	<= lock128k?1'b1:~D[7];
+		end
+    end
+end
+
+
+/* PORT EFFD */
+wire port_eff7_cs = CPU_M1 == 1 && n_iorq0 == 0 && A == 16'heff7;
+reg ext_video_16col 		= 1'b0;
+reg lock128k 				= 1'b0;
+reg ram2rom 				= 1'b0;
+reg turbo 					= 1'b0;
+reg ext_video_384x304 	= 1'b0;
+
+always @(posedge CLK_14MHZ or negedge CPU_RESET) begin
+    if (!CPU_RESET) begin
+			ext_video_16col 	<= 1'b0;
+			lock128k 			<= 1'b0;
+			ram2rom 				<= 1'b0;
+			turbo 				<= 1'b0;
+			ext_video_384x304 <= 1'b0;
+    end
+    else begin 
+		if (port_eff7_cs && CPU_WR == 0) begin
+			ext_video_16col 	<= D[0];
+			lock128k 			<= D[2];
+			ram2rom 				<= D[3];
+			turbo 				<= D[4];
+			ext_video_384x304 <= D[5];
+		end
     end
 end
 
